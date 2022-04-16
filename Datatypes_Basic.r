063d2f639f38baeb8291df0b788cec1ea1736096
Shubham Sarkar <oculusgrudge@gmail.com>
2022-04-16 20:35:03 +0530

    Data_Analytics_R_16_08_34_IST

---
+++ Datatypes_Basic.r
@@ -0,0 +1,16 @@
+# Numeric-----------------------------
+123.01
+class(123.01)
+
+
+#Character---------------------------
+class('Shubham')
+
+#logical ----------------------------
+class(TRUE)
+
+# Convert the DataTypes
+as.numeric("123")
+as.character(12345)
+as.character(TRUE)
+as.logical(12345)
