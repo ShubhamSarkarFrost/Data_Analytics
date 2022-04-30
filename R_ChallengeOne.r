362dec3be3c89f846e4b5629ec2f2a4c57f227f6
Shubham Sarkar <oculusgrudge@gmail.com>
2022-04-30 19:59:38 +0530

    Challenge One R Lesson_2022_30_APR

---
+++ R_ChallengeOne.r
@@ -0,0 +1,6 @@
+#Create a Vector and enter the year value as 2021 and change to character 
+year <- 2021
+class(year) # should be Numeric
+#Change to Character 
+year <- as.character(year)
+class(year) # should be Character 
