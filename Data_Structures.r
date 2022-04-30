ade2601c6633bc5aa166f06f1647ff292cc1d7d0
Shubham Sarkar <oculusgrudge@gmail.com>
2022-04-30 19:54:35 +0530

    R_CODE_30_APR_2022_54:07_IST

---
+++ Data_Structures.r
@@ -0,0 +1,37 @@
+# Single Element Vector Creation ----------------------------------------------
+
+#1 - Create Data for Day Sales Activity ---------------------------------------
+month <- "Sep_2019"
+month = "Sep_2019"
+month
+
+
+# See what variables are present in global namespace---------------------------
+ls()
+rm(month)
+ls()
+
+
+#Clear the screeb
+cat('\14')
+
+
+# Arrow in Opposite direction also works --------------------------------------
+"Sep_2019" -> month
+month
+
+#4. Check class and length ----------------------------------------------------
+class(month) # Equals to Character 
+length(month) # Equal to 1
+
+#5. Create Day of Month -------------------------------------------------------
+day_of_month <- 1.0
+class(day_of_month)
+
+#6. change to character 
+day_of_month <- as.character(day_of_month) # change to Character Class
+day_of_month
+class(day_of_month)
+
+
+
