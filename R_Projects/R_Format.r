5104e539c5150ac0b994d40b298dc7850d65f1a5
Shubham Sarkar <oculusgrudge@gmail.com>
2022-05-03 14:04:20 +0530

    R_FORMAT_03_05_2022

---
+++ R_Projects/R_Format.r
@@ -0,0 +1,13 @@
+#formatting numbers ----------------------------------------------------------->
+#controling the number of digits printed in the console 
+n <- 1.1234567
+n2 <- 112.34567
+
+n  <- round(n,2)
+n2 <- round(n2,3)
+
+n <- trunc(n)
+n2 <- trunc(n2)
+
+n
+n2
